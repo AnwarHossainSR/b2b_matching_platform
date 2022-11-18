7799651c75df34b36ec437c745b03744c3fcc6a1
Anwar Hossain <anwar.sarker@bjitgroup.com>
2022-11-18 08:33:57 +0600

    added request class for stor efawq

--- app/Http/Requests/Faqs/FaqStoreRequest.php
+++ app/Http/Requests/Faqs/FaqStoreRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\Faqs;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Gate;
 
 class FaqStoreRequest extends FormRequest
 {
@@ -13,7 +14,7 @@ class FaqStoreRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return Gate::allows('faq-create');
     }
 
     /**
@@ -24,7 +25,15 @@ class FaqStoreRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'question' => [
+                'required',
+                'string',
+                'unique:' . config('constants.table.faqs') . ',question',
+            ],
+            'answer' => [
+                'required',
+                'string',
+            ]
         ];
     }
 }
